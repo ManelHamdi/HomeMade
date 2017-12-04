d29fc88ff74ba11078ad232e24cd555d53e579f2
khaoula <khaoula.sliti95@gmail.com>
2017-12-04 13:28:36 +0100

    // ajouter video

--- app/src/main/java/com/example/manel/homemade/MainActivity.java
+++ app/src/main/java/com/example/manel/homemade/MainActivity.java
@@ -1,13 +1,116 @@
 package com.example.manel.homemade;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubeThumbnailView;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+    private static final String API_KEY="AIzaSyA1wl3yEp81Fg4Whh6BzwrRbiv-88Kikzo";
+    private static final String VIDEO_ID="W4hTJybfU7s";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity","khaoula");
         setContentView(R.layout.activity_main);
+        YouTubePlayerView mYouTubePlayerView=(YouTubePlayerView)findViewById(R.id.video);
+      /*  YouTubePlayer.OnInitializedListener mInitializedListener=new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d("MainActivity","init success");
+              //  youTubePlayer.loadVideo("W4hTJybfU7s");
+              // youTubePlayer.loadPlaylist("PLxr551TUsmAqhqzeeqZYB2-YxdJJXw4Ao");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        Log.d("Activity","sliti");*/
+        mYouTubePlayerView.initialize(API_KEY,this);
+
     }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+        if(!b){
+            youTubePlayer.cueVideo(VIDEO_ID);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this,"error",Toast.LENGTH_LONG).show();
+    }
+
+    private YouTubePlayer.PlaybackEventListener playbackEventListener=new YouTubePlayer.PlaybackEventListener() {
+        @Override
+        public void onPlaying() {
+
+        }
+
+        @Override
+        public void onPaused() {
+
+        }
+
+        @Override
+        public void onStopped() {
+
+        }
+
+        @Override
+        public void onBuffering(boolean b) {
+
+        }
+
+        @Override
+        public void onSeekTo(int i) {
+
+        }
+    };
+
+    private YouTubePlayer.PlayerStateChangeListener playerStateChangeListener=new YouTubePlayer.PlayerStateChangeListener() {
+        @Override
+        public void onLoading() {
+
+        }
+
+        @Override
+        public void onLoaded(String s) {
+
+        }
+
+        @Override
+        public void onAdStarted() {
+
+        }
+
+        @Override
+        public void onVideoStarted() {
+
+        }
+
+        @Override
+        public void onVideoEnded() {
+
+        }
+
+        @Override
+        public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+        }
+    };
+
 }
---
+++ app/src/main/java/com/example/manel/homemade/YoutubeConfig.java
@@ -0,0 +1,8 @@
+package com.example.manel.homemade;
+
+/**
+ * Created by Dell on 02/12/2017.
+ */
+
+public class YoutubeConfig {
+}
